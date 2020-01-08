7671b8d597078bc64ed9200e1276b295db2343ca
Paola Hinostroza Trujillo <paola.hinostroza.trujillo.st@everis.com>
2020-01-08 17:26:20 -0300

    commit 8/1 notebook

--- src/app/servicios/heroes.service.ts
+++ src/app/servicios/heroes.service.ts
@@ -71,6 +71,23 @@ export class HeroesService {
        return this.heroes[idx]
      }
 
+     getBuscarHeroes(termino: string ) : Heroe[] {
+        
+      let heroeArr: Heroe[]=[];
+
+      termino= termino.toLowerCase();
+
+      for(let heroe of this.heroes){
+        let nombre = heroe.nombre.toLowerCase();
+
+        if(nombre.indexOf(termino) >=0 ) {
+          heroeArr.push(heroe)
+        }
+      }
+return heroeArr;
+
+     }
+
 
 
 }
